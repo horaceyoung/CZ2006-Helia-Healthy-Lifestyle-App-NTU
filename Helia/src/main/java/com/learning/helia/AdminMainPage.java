f873b25a00a6391a1593566b66dd46aa44595b3a
Yuehann <42796162+Yuehann@users.noreply.github.com>
2018-10-03 23:33:52 +0800

    CreateEvent & Admin Section

---
+++ Helia/src/main/java/com/learning/helia/AdminMainPage.java
@@ -0,0 +1,67 @@
+package com.learning.helia;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AdminMainPage extends AppCompatActivity {
+
+    public Button toBeApproved;
+
+    public void initToBeApproved(){
+        toBeApproved=(Button)findViewById(R.id.toBeApprovedBut);
+        toBeApproved.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent toBeApproved = new Intent(AdminMainPage.this, eventsToBeApproved.class);
+                startActivity(toBeApproved);
+            }
+        });
+
+    }
+
+
+
+    public Button approved;
+
+    public void initApproved(){
+        approved=(Button)findViewById(R.id.approvedBut);
+        approved.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent approved = new Intent(AdminMainPage.this, eventsApproved.class);
+                startActivity(approved);
+            }
+        });
+
+    }
+
+
+    public Button logOut;
+
+    public void initLogOut(){
+        logOut=(Button)findViewById(R.id.logOutBut);
+        logOut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent logOut = new Intent(AdminMainPage.this, adminLogOut.class);
+                startActivity(logOut);
+            }
+        });
+
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_main_page);
+        initToBeApproved();
+        initApproved();
+        initLogOut();
+    }
+}
---
+++ Helia/src/main/java/com/learning/helia/adminLogOut.java
@@ -0,0 +1,13 @@
+package com.learning.helia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class adminLogOut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_log_out);
+    }
+}
