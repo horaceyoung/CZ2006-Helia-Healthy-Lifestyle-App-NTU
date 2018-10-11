016fa1e586df062e193faf4f345eb751f75d668f
splashingsplashes <splashingsplashes@gmail.com>
2018-10-11 16:12:20 +0800

    InterestedEventsRecyclerView

--- Helia/app/src/main/java/com/zy/helia/MeIrInterested.java
+++ Helia/app/src/main/java/com/zy/helia/MeIrInterested.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.zy.helia.Activities.EventListAdapter;
 
 
 /**
@@ -18,6 +22,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MeIrInterested extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -58,13 +67,29 @@ public class MeIrInterested extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+        //newly added section
+        View view = inflater.inflate(R.layout.fragment_me__ir__interested, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.InterestedEventsList);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager((mLayoutManager));
+
+        mAdapter = new EventListAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+        //end of newly added section
+
+
         return inflater.inflate(R.layout.fragment_me__ir__interested, container, false);
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
