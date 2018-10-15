cc7bd94b44e2e35afac6930a1fddbd7b3302b964
splashingsplashes <splashingsplashes@gmail.com>
2018-10-15 19:28:08 +0800

    InterestedRV

---
+++ Helia/app/src/main/java/com/zy/helia/Activities/MeRegisteredEventListAdapter.java
@@ -0,0 +1,13 @@
+package com.zy.helia.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeRegisteredEventListAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me_registered_event_list_adapter);
+    }
+}
