3b3f13056bf469d1510a7d00b36ec4c0d6d0fd14
Yuehann <zyh990424@gmail.com>
2018-10-19 15:05:47 +0800

    eventFragReverted

--- Helia/app/src/main/java/com/zy/helia/EventFragmentAdapter.java
+++ Helia/app/src/main/java/com/zy/helia/EventFragmentAdapter.java
@@ -1,23 +1,18 @@
-package com.zy.helia;
+        package com.zy.helia;
 
-import android.content.Context;
-import android.content.Intent;
-<<<<<<< HEAD
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-=======
->>>>>>> 87093b909797f1ff279ac9d861a62a049b3ff1f2
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.support.annotation.NonNull;
+        import android.support.v7.widget.RecyclerView;;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Button;
+        import android.widget.TextView;
 
 
-import com.zy.helia.Activities.EventDetail;
-import java.util.ArrayList;
+        import com.zy.helia.Activities.EventDetail;
+        import java.util.ArrayList;
 
 
 public class EventFragmentAdapter extends RecyclerView.Adapter<EventFragmentAdapter.ViewHolder> {
@@ -25,27 +20,13 @@ public class EventFragmentAdapter extends RecyclerView.Adapter<EventFragmentAdap
     private static Context context;
     private int totalCount;
     private ArrayList<String> EventName;
-<<<<<<< HEAD
 
     private Button button;
 
-=======
->>>>>>> 87093b909797f1ff279ac9d861a62a049b3ff1f2
-
-    private Button button;
-
-<<<<<<< HEAD
-
-
-    public EventFragmentAdapter(Context context, ArrayList<String> EventName) {
-
-        this.context = context;
-=======
     // two parameter for this class - context and an ArrayList containing the EventName to be in the recyclerView
     public EventFragmentAdapter(Context context, ArrayList<String> EventName) {
         this.context = context;
         // get the number of EventName when the class is constructed
->>>>>>> 87093b909797f1ff279ac9d861a62a049b3ff1f2
         this.totalCount = EventName.size();
         this.EventName = EventName;
     }
@@ -64,45 +45,13 @@ public class EventFragmentAdapter extends RecyclerView.Adapter<EventFragmentAdap
     public EventFragmentAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_event_adapter, parent, false);
         ViewHolder vh = new ViewHolder(v);
-
-
-
         return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull final EventFragmentAdapter.ViewHolder holder, final int position) {
-<<<<<<< HEAD
-
-        Toast.makeText(context,"WuZiang",Toast.LENGTH_LONG).show();
-        Toast.makeText(context, "onBindViewHolder", Toast.LENGTH_SHORT).show();
-
-        try {
-            DatabaseHelp db = new DatabaseHelp(context);
-            eventds = db.viewPendingEvents();
-            if(eventds==null)
-                Toast.makeText(context,"Null",Toast.LENGTH_LONG).show();
-        }
-        catch (Exception e){
-            Toast.makeText(context,"Error",Toast.LENGTH_LONG).show();
-        }
-        while (eventds.moveToNext())
-        {
-            int eventIndex = eventds.getColumnIndex("Event_Name");
-            String eventName = eventds.getString(eventIndex);
-            Toast.makeText(context,eventName,Toast.LENGTH_LONG).show();
-            EventName.add(eventName);
-        }
-        button.setText(EventName.get(position));
-
-
-
         holder.button.setText(EventName.get(position));
 
-=======
-        holder.button.setText(EventName.get(position));
-
->>>>>>> 87093b909797f1ff279ac9d861a62a049b3ff1f2
         holder.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -116,10 +65,6 @@ public class EventFragmentAdapter extends RecyclerView.Adapter<EventFragmentAdap
 
             }
         });
-
-
-
-
     }
 
     @Override
