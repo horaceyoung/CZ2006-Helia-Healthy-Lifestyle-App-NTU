7c2e33ae0586560657db294bc5ca95ff6ba57cd9
Yuehann <zyh990424@gmail.com>
2018-10-22 11:48:01 +0800

    approve

--- Helia/app/src/main/java/com/zy/helia/Activities/eventsApproved.java
+++ Helia/app/src/main/java/com/zy/helia/Activities/eventsApproved.java
@@ -1,5 +1,6 @@
 package com.zy.helia.Activities;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.zy.helia.Event_Data.DatabaseHelp;
@@ -14,14 +16,16 @@ import com.zy.helia.R;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class eventsApproved extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private Cursor eventds;
+
     private ArrayList<String> EventName = new ArrayList<>();
+    private List<Integer> EventID = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +36,7 @@ public class eventsApproved extends AppCompatActivity {
 
         DatabaseHelp dbHelper = new DatabaseHelp(this);
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        Cursor eventCursor = dbHelper.viewMyEvent(LoginActivity.getUserID());
+        Cursor eventCursor = dbHelper.viewApprovedEvents();
 
         Log.d("EventActivity", "This is called" );
         while (eventCursor.moveToNext())
@@ -41,6 +45,10 @@ public class eventsApproved extends AppCompatActivity {
             String eventName = eventCursor.getString(eventIndex);
             Log.d("EventActivity", "Event name 1233" +eventName);
             EventName.add(eventName);
+
+            int IDIndex = eventCursor.getColumnIndex("Event_ID");
+            int eventID = eventCursor.getInt(IDIndex);
+            EventID.add(eventID);
         }
         db.close();
 
@@ -50,9 +58,10 @@ public class eventsApproved extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager((mLayoutManager));
 
-        mAdapter = new EventsToBeAdapter(getBaseContext(), EventName);
+        mAdapter = new eventsApprovedAdapter(getBaseContext(), EventName, EventID);
         mRecyclerView.setAdapter(mAdapter);
 
     }
+
 
 }
--- Helia/app/src/main/java/com/zy/helia/Activities/eventsToBeApproved.java
+++ Helia/app/src/main/java/com/zy/helia/Activities/eventsToBeApproved.java
@@ -1,5 +1,6 @@
 package com.zy.helia.Activities;
 
+import android.content.Intent;
 import android.database.Cursor;
         import android.database.sqlite.SQLiteDatabase;
         import android.os.Bundle;
@@ -7,21 +8,24 @@ import android.database.Cursor;
         import android.support.v7.widget.LinearLayoutManager;
         import android.util.Log;
         import android.view.View;
-        import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.ImageButton;
 
         import com.zy.helia.Event_Data.DatabaseHelp;
         import com.zy.helia.R;
         import android.support.v7.widget.RecyclerView;
 
         import java.util.ArrayList;
+import java.util.List;
 
 public class eventsToBeApproved extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private Cursor eventds;
+
     private ArrayList<String> EventName = new ArrayList<>();
+    private List<Integer> EventID = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +36,7 @@ public class eventsToBeApproved extends AppCompatActivity {
 
         DatabaseHelp dbHelper = new DatabaseHelp(this);
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        Cursor eventCursor = dbHelper.viewMyEvent(LoginActivity.getUserID());
+        Cursor eventCursor = dbHelper.viewPendingEvents(db);
 
         Log.d("EventActivity", "This is called" );
         while (eventCursor.moveToNext())
@@ -41,6 +45,10 @@ public class eventsToBeApproved extends AppCompatActivity {
             String eventName = eventCursor.getString(eventIndex);
             Log.d("EventActivity", "Event name 1233" +eventName);
             EventName.add(eventName);
+
+            int IDIndex = eventCursor.getColumnIndex("Event_ID");
+            int eventID = eventCursor.getInt(IDIndex);
+            EventID.add(eventID);
         }
         db.close();
 
@@ -50,9 +58,10 @@ public class eventsToBeApproved extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager((mLayoutManager));
 
-        mAdapter = new EventsToBeAdapter(getBaseContext(), EventName);
+        mAdapter = new EventsToBeAdapter(getBaseContext(), EventName, EventID);
         mRecyclerView.setAdapter(mAdapter);
 
     }
+
 
 }
