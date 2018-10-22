3899d052a8bb6c9a06587b7a054d7f7845702c9d
cyberwander <cyberwanderwayne@gmail.com>
2018-10-23 01:19:10 +0800

    update

--- Helia/app/src/main/res/raw/kml_file_delete_noline.py
+++
@@ -1,7 +0,0 @@
-infile = 'new_health.txt'
-outfile  = 'new_health_noline.txt'
-fin = open(infile)
-fout = open(outfile, "w+")
-for line in fin.readlines():
-    if not line.strip(): continue
-    fout.write(line)
